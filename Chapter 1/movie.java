b6ca1cc01412fbe5f28e5eef6af4f22d652dec37
Trixetterainmontallana <trixetterainalvaradomontallana@gmail.com>
2024-08-14 23:55:40 +0800

    Add files via upload

---
+++ Chapter 1/movie.java
@@ -0,0 +1,7 @@
+public class Movie{
+    public static void main (String[]args){
+        System.out.println("Trixette Rain's Favorite Movie Quote:");
+        System.out.println("Love means never having to say you're sorry.");
+        System.out.println("- Jennifer Cavalleri (1970) ");
+        }
+}
