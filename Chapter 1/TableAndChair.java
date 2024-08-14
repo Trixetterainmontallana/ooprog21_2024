ba6e97aced968f39158945b159f579409c4dc5fd
Trixetterainmontallana <trixetterainalvaradomontallana@gmail.com>
2024-08-14 23:34:50 +0800

    Add files via upload

---
+++ Chapter 1/TableAndChair.java
@@ -0,0 +1,16 @@
+public class TableAndChair {
+    public static void main(String[] args) {
+        String[] tablechairs = {
+        "X                    X",
+        "X                    X",
+        "X     XXXXXXXXXX     X",
+        "XXXXX X        X XXXXX",  
+        "X   X X        X X   X",
+        "X   X X        X X   X",    
+        };
+
+        for (String line : tablechairs) {
+            System.out.println(line);
+        }
+    }
+}
