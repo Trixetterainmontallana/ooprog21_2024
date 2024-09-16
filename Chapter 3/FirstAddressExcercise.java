aad23d5b8b5226774851b4d94b68bd844f0de903
Trixetterainmontallana <trixetterainalvaradomontallana@gmail.com>
2024-09-16 20:18:22 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,18 @@
+public class FirstAddressExcercise
+{
+    public static void displayAddress() 
+    {
+        System.out.println("Samsung Digital City");
+        System.out.println("129 Samsung-ro, Yeongtong-gu");
+        System.out.println("Suwon-si, Gyeonggi-do 16677");
+        System.out.println("South Korea");
+        System.out.println("Company: Samsung Electronics Co., Ltd.");
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println("First Java application");
+       
+        displayAddress();
+    }
+}
